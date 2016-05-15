d767cde62ab1ee891e6a49736b0ea08f7aea86af
Jose E Lopez <p0p.thr0w@gmail.com>
2016-05-15 09:12:50 -0500

    Improvements to the how the game works in landscape mode. Started implementing the share app service.

--- app/src/main/java/rocks/throw20/funwithcountries/GameActivity.java
+++ app/src/main/java/rocks/throw20/funwithcountries/GameActivity.java
@@ -47,17 +47,7 @@ public class GameActivity extends AppCompatActivity {
 
 
     }
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
 
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-        }
-    }
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             exitByBackKey();
